a92de3e3dfb5a18d3715bfd00770443857f41a38
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-06-26 20:07:35 +0700

    Create composer.php

---
+++ composer.php
@@ -0,0 +1,21 @@
+{
+    "name": "xpocketmc/xpocketmprotocols",
+    "type": "library",
+    "require-dev": {
+        "php": "^8.1"
+        "pocketmine/nbt": "^1.0.0"
+    },
+    "license": "lgpl-3.0-only",
+    "autoload": {
+        "psr-4": {
+            "xpocketmc\\xpocketmprotocols\\": "src/"
+        }
+    },
+    "authors": [
+        {
+            "name": "SketchProX",
+            "email": "123lah123lah@gmail.com"
+        }
+    ],
+    "require": {}
+}
